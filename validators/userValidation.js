f2b66d16408b967e0a70c405d95c8ccfe8d7cf91
sahilbista205@gmail.com <sahilbista205@gmail.com>
2025-08-31 15:10:27 +0545

    feat : Added validation for user input for user registration and created a validaiton middlware to handle validation errors

---
+++ validators/userValidation.js
@@ -0,0 +1,47 @@
+import { body } from "express-validator";
+
+export const registerValidator = [
+    body("email")
+        .trim()
+        .normalizeEmail()
+        .notEmpty()
+        .unique()
+        .isEmail()
+        .withMessage('Please entet a valid email'),
+    
+    body("password")
+        .trim()
+        .isLength({gt : 6})
+        .withMessage('Password must be at least 7 letters'),
+    
+    body("firstName")
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage('First Name is required'),
+    
+    body("lastName")
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage('First Name is required'),
+
+    body("phoneNumber")
+        .trim()
+        .matches(/^[0-9]{10}$/)
+        .withMessage('Password must be at least 10 digits'),
+
+    body("roles")
+        .optional()
+        .isArray()
+        .withMessage("Roles must be an array")
+        .custom((roles)=>{
+            const allowedRoles = ['User','Admin']
+            for (const role of roles){
+                if (!allowedRoles.includes(role)){
+                    throw new Error('Invalid role');
+                }
+            }
+            return true;
+        })
+]
