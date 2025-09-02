86d3f75c1ea387b90e2ad54c58c639a1842d2b90
sahilbista205@gmail.com <sahilbista205@gmail.com>
2025-09-02 14:42:09 +0545

    Feat : Added validator to validate incoming request object to create a task

---
+++ controllers/taskController.js
@@ -0,0 +1,40 @@
+import { ProjectModel } from "../models/Project.js";
+import { TaskModel } from "../models/Task";
+
+export const createTask = async(req , res)=>{
+    try{
+        const userId = req.userId;
+        const {name,description, assignedTo, projectId, dueDate} = req.body;
+        const foundProject = await ProjectModel.findById(projectId);
+        if(!foundProject){
+            return res.status(404).json({msg:'No such project available'});
+        }
+        const owner = foundProject.owner;
+        if(owner.toString() !== userId){
+            return res.status(403).json({msg:'User unauthorized to add task to this project'});
+        }
+        const projectMembers = foundProject.members;
+        const validMembers = [];
+        const invalidMembers = [];
+        await promise.all(
+            assignedTo.map((assignees)=>{
+                if(projectMembers.includes(assignees)){
+                    validMembers.push(assignees);
+                }else{
+                    invalidMembers.push(assignees);
+                }
+            })
+        );
+        const duplicate = await TaskModel.findOne({name, description, projectId});
+        if(duplicate){
+            return res.status(409).json({msg:'The same task already exists in the project'});
+        }
+        const newTask = await TaskModel.create({
+            name, description,  assignedTo : validMembers, projectId, dueDate
+        });
+        return res.json({msg : 'Task created successfully', data : newTask, invalidMembers : invalidMembers});
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({msg:'Internal Server error'})
+    }   
+}
---
+++ validators/taskValidation.js
@@ -0,0 +1,45 @@
+import { body } from "express-validator";
+import mongoose from "mongoose";
+
+export const createTaskValidator = [
+    body("name")
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage("Please enter a valid task name"),
+
+    body("description")
+        .trim()
+        .escape()
+        .isLength({min : 20})
+        .withMessage("Task description must be at least 20 letters"),
+    
+    body("assignedTo")
+        .optional()
+        .isArray()
+        .custom((assignedTo)=>{
+            for (const member of assignedTo){
+                if(!mongoose.Types.ObjectId.isValid(member)){
+                    throw new Error (`${member} is not a valid mongoID`)
+                }
+            }
+            return true
+        }),
+    
+    body("projectId")
+        .notEmpty()
+        .isMongoId()
+        .withMessage('Project Id must be a valid mongoose object'),
+    
+    body("dueDate")
+        .isISO8601().
+        withMessage("Due date must be a valid date in ISO8601 format")
+        .custom((value) => {
+        const inputDate = new Date(value);
+        const now = new Date();
+        if (inputDate <= now) {
+            throw new Error("Due date must be in the future");
+        }
+        return true;
+        }) 
+]
